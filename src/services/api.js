3b8c9597d1579c1c2c98dfbd245bb526b35afdbb
Amit prakash <amitstudy195@gmail.com>
2026-04-28 13:23:13 +0530

    payment gateway, google sign in

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://movie-show-backend-lr9n.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
